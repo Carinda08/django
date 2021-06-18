f130cb8f06188a8f8f431d8dcb4a2e524c7045fb
Carinda08 <carinda08@outlook.com>
2021-06-18 23:04:51 +0800

    add html and urls

--- prj_stu0/users/views.py
+++ prj_stu0/users/views.py
@@ -1,3 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    m = request.method
+
+    if m == "GET":
+        return render(request, 'login.html')
+    else:
+        return HttpResponse('Login')
+
+
+def register(request):
+    return HttpResponse('Register')
