1854f23478ff9f501dd8d0566c8febee7c69e34c
dengbj <dengbj@qdaoe.cn>
2021-06-25 16:43:54 +0800

    add other data  type examples

--- prj_stu0/users/models.py
+++ prj_stu0/users/models.py
@@ -7,6 +7,31 @@ class Students(models.Model):
     #name
     uname = models.CharField(max_length=30, unique=True)
     upwd = models.CharField(max_length=30)
+    """
+    other type:
+    t1 = models.TextField() #* long string 
+    t2 = models.DateField()
+    t3 = models.DateTimeField()
+    t4 = models.BooleanField()
+    t5 = models.IntegerField()
+    t6 = models.PositiveIntegerField()
+    t7 = models.DecimalField()
+    t8 = models.ImageField() #* depend on Pillow
+    t9 = models.FileField()
+    o1 = models.AutoField()
+    o2 = models.ForeignKey()   #* 1:n
+    o3 = models.ManyToManyField() #* n:n
+    o4 = models.EmailField()
+    o5 = models.UUIDField()
+    """
 
     def __str__(self) -> str:
         return u"Stu:%s"%self.uname
+
+    """
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'ModelName'
+        verbose_name_plural = 'ModelNames'
+    """
